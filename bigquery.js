5b36be34606d319a2591e266be95d42e81c98f0e
anatoly techtonik <techtonik@gmail.com>
2016-10-23 21:56:33 +0300

    Always open new tab

--- bigquery.js
+++ bigquery.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.update({ url: "https://bigquery.cloud.google.com/" });
+    chrome.tabs.create({ url: "https://bigquery.cloud.google.com/" });
 });
